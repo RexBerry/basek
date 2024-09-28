48f5c40137ae62805ad93f9f69c1c57a6f9e708c
Rex Berry <59031902+RexBerry@users.noreply.github.com>
2024-09-28 01:59:43 -0500

    Improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,113 @@
 import { encodeText, decodeText } from "@root/lib/main";
-import { onMount } from "solid-js";
+import { createSignal } from "solid-js";
 
 function App() {
+    const [decodedText, setDecodedText] = createSignal("");
+    const [encodedText, setEncodedText] = createSignal("");
+    const [charset, setCharset] = createSignal(
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~",
+    );
+
+    const [decodeRate, setDecodeRate] = createSignal(0);
+    const [encodeRate, setEncodeRate] = createSignal(0);
+
     let decodedTextArea!: HTMLTextAreaElement;
     let encodedTextArea!: HTMLTextAreaElement;
     let charsetTextArea!: HTMLTextAreaElement;
 
-    onMount(() => {
-        charsetTextArea.value =
-            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~";
-    });
+    const textEncoder = new TextEncoder();
 
     return (
-        <div class="m-5">
-            <h2>Decoded Text</h2>
-            <textarea
-                class="font-mono text-sm w-1/2 h-48 border border-gray-500"
-                ref={decodedTextArea}
-            ></textarea>
+        <div class="m-auto flex flex-col gap-4">
             <div>
+                <h2>Decoded Text</h2>
+                <textarea
+                    class="w-1/2 h-48"
+                    ref={decodedTextArea}
+                    oninput={(e) => setDecodedText(e.target.value)}
+                >
+                    {decodedText()}
+                </textarea>
+                <p class="text-sm text-gray-600">
+                    {textEncoder.encode(decodedText()).length.toLocaleString()} bytes •{" "}
+                    {decodeRate().toLocaleString()} bytes/second decoded
+                </p>
+            </div>
+            <div class="flex flex-row gap-2">
                 <button
-                    class="mx-auto border border-gray-500 bg-gray-200"
                     onclick={() => {
-                        encodedTextArea.value = encodeText(
-                            decodedTextArea.value,
-                            charsetTextArea.value,
-                        );
+                        try {
+                            const text = decodedText();
+                            const charsetValue = charset();
+                            const timeBegin = performance.now();
+                            const encoded = encodeText(text, charsetValue);
+                            const timeEnd = performance.now();
+                            const textLength = textEncoder.encode(text).length;
+                            const elapsedTime = Math.max(
+                                1e-4,
+                                Math.round(1000 * (timeEnd - timeBegin)) / 1e6,
+                            );
+                            setEncodeRate(Math.round(textLength / elapsedTime));
+                            encodedTextArea.value = encoded;
+                            setEncodedText(encodedTextArea.value);
+                        } catch (ex) {
+                            alert(ex);
+                        }
                     }}
                 >
                     Encode
                 </button>
                 <button
-                    class="mx-auto border border-gray-500 bg-gray-200"
                     onclick={() => {
-                        decodedTextArea.value = decodeText(
-                            encodedTextArea.value,
-                            charsetTextArea.value,
-                        );
+                        try {
+                            const text = encodedText();
+                            const charsetValue = charset();
+                            const timeBegin = performance.now();
+                            const decoded = decodeText(text, charsetValue);
+                            const timeEnd = performance.now();
+                            const textLength = textEncoder.encode(decoded).length;
+                            const elapsedTime = Math.max(
+                                1e-4,
+                                Math.round(1000 * (timeEnd - timeBegin)) / 1e6,
+                            );
+                            setDecodeRate(Math.round(textLength / elapsedTime));
+                            decodedTextArea.value = decoded;
+                            setDecodedText(decodedTextArea.value);
+                        } catch (ex) {
+                            alert(ex);
+                        }
                     }}
                 >
                     Decode
                 </button>
             </div>
-            <h2>Encoded Text</h2>
-            <textarea
-                class="font-mono text-sm break-all w-1/2 h-48 border border-gray-500"
-                ref={encodedTextArea}
-            ></textarea>
-            <h2>Charset</h2>
-            <textarea
-                class="font-mono text-sm break-all w-1/2 h-16 border border-gray-500"
-                ref={charsetTextArea}
-            ></textarea>
+            <div>
+                <h2>Encoded Text</h2>
+                <textarea
+                    class="break-all w-1/2 h-48"
+                    ref={encodedTextArea}
+                    oninput={(e) => setEncodedText(e.target.value)}
+                >
+                    {encodedText()}
+                </textarea>
+                <p class="text-sm text-gray-600">
+                    {textEncoder.encode(encodedText()).length.toLocaleString()} bytes •{" "}
+                    {encodeRate().toLocaleString()} bytes/second encoded
+                </p>
+            </div>
+            <div>
+                <h2>Character Set</h2>
+                <textarea
+                    class="break-all w-1/2 h-12"
+                    ref={charsetTextArea}
+                    oninput={(e) => setCharset(e.target.value)}
+                >
+                    {charset()}
+                </textarea>
+                <p class="text-sm text-gray-600">
+                    {charset().length.toLocaleString()} characters
+                </p>
+            </div>
         </div>
     );
 }
